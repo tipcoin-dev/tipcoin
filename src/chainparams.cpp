ad94ae371f0769f8b11b1e2c6913652323cf2481
arubinofaux <arubinofaux@gmail.com>
2021-05-27 23:32:21 -0600

    uncomment temp seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -139,7 +139,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        // vSeeds.emplace_back("dnsseed.tipcoin.us");
+        vSeeds.emplace_back("dnsseed.tipcoin.us");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,43);  // J
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
