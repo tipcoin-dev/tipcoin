9ffd4b72add1444ea9b18a4433940a4afbd09700
arubinofaux <arubinofaux@gmail.com>
2021-06-08 13:04:02 -0600

    gitian use MacOSX10.11.sdk

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -56,8 +56,8 @@ def build():
     os.chdir('gitian-builder')
     os.makedirs('inputs', exist_ok=True)
 
-    subprocess.check_call(['wget', '-O', 'inputs/osslsigncode-2.0.tar.gz', 'https://github.com/decryp2kanon/apple_sdk/releases/download/200923/osslsigncode-2.0.tar.gz'])
-    subprocess.check_call(["echo '5a60e0a4b3e0b4d655317b2f12a810211c50242138322b16e7e01c6fbb89d92f inputs/osslsigncode-2.0.tar.gz' | sha256sum -c"], shell=True)
+    subprocess.check_call(['wget', '-N', '-P', 'inputs', 'https://github.com/mraksoll4/osslsigncode-1.7.1.tar.gz-1/raw/master/osslsigncode-1.7.1.tar.gz'])
+    subprocess.check_call(['wget', '-N', '-P', 'inputs', 'https://raw.githubusercontent.com/mraksoll4/osslsigncode-1.7.1.tar.gz-1/master/osslsigncode-Backports-to-1.7.1.patch'])
     subprocess.check_call(['make', '-C', '../tipcoin/depends', 'download', 'SOURCES_PATH=' + os.getcwd() + '/cache/common'])
 
     if args.linux:
@@ -211,7 +211,7 @@ def main():
     args.macos = 'm' in args.os
 
     # Disable for MacOS if no SDK found
-    if args.macos and not os.path.isfile('gitian-builder/inputs/MacOSX10.14.sdk.tar.gz'):
+    if args.macos and not os.path.isfile('gitian-builder/inputs/MacOSX10.11.sdk.tar.gz'):
         print('Cannot build for MacOS, SDK does not exist. Will build for other OSes')
         args.macos = False
 
